bdd997a88b03d19f0756e5d88411c1157a8ee2b2
Beatriz Kamien Tehzy <141372642+Beaktz@users.noreply.github.com>
2025-10-23 09:35:12 -0300

    Update Resposta.java

--- src/Resposta.java
+++ src/Resposta.java
@@ -1,11 +1,18 @@
+import java.io.Serializable;
+
 public class Resposta extends Comunicado {
     private Integer contagem;
 
-    public Resposta(int contagem) {
+    public Resposta(int contagem){
         this.contagem = contagem;
     }
 
-    public Integer getContagem() {
-        return contagem;
+    public Integer getContagem(){
+        return this.contagem;
+    }
+
+    @Override
+    public String toString(){
+        return "Resposta [Contagem: " + contagem + "]";
     }
 }
